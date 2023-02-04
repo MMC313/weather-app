092480080a148eb6bbef388b107ef09df8da985d
MartianMooCow <NathenL2832@gmail.com>
2023-02-04 01:11:52 -0500

    add search icon

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const test = document.getElementById("test")
-
-
 async function getCoord(location){
     const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=fb9354257ea335d5387ad134ddc55aa7`)
     const responseJson = await response.json();
@@ -14,13 +11,14 @@ async function getCoord(location){
 async function currentWeather(latitude,longtitude){
     const weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&limit=1&appid=fb9354257ea335d5387ad134ddc55aa7`)
     const weatherJson = await weather.json();
+    
     console.log(weatherJson)
 }
 
 
 async function getData(){
  const coords = await getCoord("new york");
- const currentWeather = await currentWeather(coords.lat,coords.lon);
+ const weather = await currentWeather(coords.lat,coords.lon);
  
 }
 
